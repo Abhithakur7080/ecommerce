7265acc8147b55ceb02673c3808ba7008ed93af1
Abhijeet Kumar <abhijeetthakur7080@gmail.com>
2024-03-29 22:11:03 +0530

    cloudinary setup completed

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,23 @@
+import cloudinary from "cloudinary";
+
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.API_KEY,
+  api_secret: process.env.API_SECRET,
+});
+
+const cloudinaryUploading = async (fileToUploads) => {
+  return new Promise((resolve) => {
+    cloudinary.uploader.upload(fileToUploads, (result) => {
+      resolve(
+        {
+          url: result.secure_url,
+        },
+        {
+          resource_type: "auto",
+        }
+      );
+    });
+  });
+};
+export { cloudinaryUploading };
